3dace8fbfa1e1eacb44816af543a23275ec3b44f
Tanveer Najib <tanveernajib@gmail.com>
2025-11-08 14:43:04 +0000

    Stupid CORS

--- api/trakt/list/[username]/[slug].ts
+++ api/trakt/list/[username]/[slug].ts
@@ -1,8 +1,21 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
-import { getListItems } from '../../../../lib/trakt';
+import { getListItems } from '../../../../lib/trakt'
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const { username, slug } = req.query;
-  let list = await getListItems(username?.toString(), slug?.toString());
-  return res.json(list);
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end()
+  }
+
+  const { username, slug } = req.query
+
+  try {
+    const list = await getListItems(username?.toString(), slug?.toString())
+    return res.status(200).json(list)
+  } catch (error: any) {
+    return res.status(500).json({ error: error.message })
+  }
 }

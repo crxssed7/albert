8b54d1bbecd222757478561eb61da8b94535238f
Tanveer <48864420+crxssed7@users.noreply.github.com>
2026-02-28 10:19:42 +0000

    Add User-Agent header to Trakt API requests

--- lib/trakt.ts
+++ lib/trakt.ts
@@ -1,5 +1,7 @@
 import { TraktListItem } from "./types";
 
+const USER_AGENT = 'Albert/1.0'
+
 export async function getListItems(username: string, slug: string): Promise<TraktListItem[]> {
   const response = await fetch(`https://api.trakt.tv/users/${username}/lists/${slug}/items?extended=full,images`, {
     method: 'GET',
@@ -8,7 +10,8 @@ export async function getListItems(username: string, slug: string): Promise<Trak
       'Accept': 'application/json',
       'trakt-api-version': '2',
       // Go on then. Steal it. I don't care
-      'trakt-api-key': '80385a95267fd85c22d380c508ee1aa687fc0aba899e3433a7df977ce0062e56'
+      'trakt-api-key': '80385a95267fd85c22d380c508ee1aa687fc0aba899e3433a7df977ce0062e56',
+      'User-Agent': USER_AGENT
     }
   })
   if (!response.ok) {
@@ -24,7 +27,8 @@ export async function getWatching(username: string): Promise<object | null> {
       'Content-Type': 'application/json',
       'Accept': 'application/json',
       'trakt-api-version': '2',
-      'trakt-api-key': '80385a95267fd85c22d380c508ee1aa687fc0aba899e3433a7df977ce0062e56'
+      'trakt-api-key': '80385a95267fd85c22d380c508ee1aa687fc0aba899e3433a7df977ce0062e56',
+      'User-Agent': USER_AGENT
     }
   })
   if (response.status !== 200) {
